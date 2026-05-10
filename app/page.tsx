196a9f5f1f4df086bb28036a66b6da11c6b5aebe
shaggyvision <137138189+shaggyvision@users.noreply.github.com>
2026-05-09 21:29:05 -0600

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: '100vh',
+      background: 'linear-gradient(to bottom right,#4c1d95,#6d28d9)',
+      color: 'white',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1 style={{fontSize:'3rem'}}>Happy Moments Xudapu</h1>
+      <p>Sistema automático de felicitaciones</p>
+    </main>
+  )
+}
